cbba0104738bad2464a8fbe5a7824a486ff69ff7
liuyi <liuxy0551@qq.com>
2019-06-21 11:50:58 +0800

    deploy

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   dev: {
 
     // Paths
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: '/',
     assetsPublicPath: '/',
     proxyTable: {},
 
@@ -49,7 +49,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
-    assetsSubDirectory: '',
+    assetsSubDirectory: 'static',
     assetsPublicPath: '/huobi-trade-admin',
 
     /**
