5d19179d94920d402155f12f3a1b83e52690a38a
liuyi <liuxy0551@qq.com>
2019-06-21 11:42:30 +0800

    deploy

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: './huobi-trade-admin',
 
     /**
      * Source Maps
