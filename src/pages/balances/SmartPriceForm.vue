2d55a7d1028226cf84153007688126c8e2388d52
sen <578595193@qq.com>
2019-07-09 20:44:39 +0800

    fix

--- src/pages/balances/SmartPriceForm.vue
+++ src/pages/balances/SmartPriceForm.vue
@@ -30,7 +30,7 @@
             <el-card v-for="(balanceSmart, index) in form.balance_smarts" :key="index" style="margin-bottom: 10px">
               <div slot="header" class="clearfisx" style="margin-bottom: 15px">
                 <span class="fl">{{balanceSmart.trade_symbol.base_currency.toUpperCase()}} / {{balanceSmart.trade_symbol.quote_currency.toUpperCase()}}</span>
-                <span class="fr" style="color: #999" v-if="balanceSmart.profit">约收益【{{balanceSmart.profit.toFixed(10)}}%】</span>
+                <span class="fr" style="color: #999" v-if="balanceSmart.profit">约收益【{{balanceSmart.trade_symbol.quote_currency.toUpperCase()}} {{balanceSmart.profit.toFixed(10)}}】</span>
               </div>
               <el-form-item class="el-form-margin" label="起始买入价格">
                 <el-input v-model="balanceSmart.open_price" placeholder="起始买入价格"></el-input>
